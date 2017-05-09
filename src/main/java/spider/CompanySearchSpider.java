e1d3a3f55df80e7ecd5589df51415e8a7597a1d8
janke <keven_susu@126.com>
2017-05-09 17:23:41 +0800

    公司搜索爬虫 && 公司员工评价爬虫

---
+++ src/main/java/spider/CompanySearchSpider.java
@@ -0,0 +1,48 @@
+package spider;
+
+import java.util.Iterator;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+import us.codecraft.webmagic.Page;
+import us.codecraft.webmagic.Site;
+import us.codecraft.webmagic.Spider;
+import us.codecraft.webmagic.processor.PageProcessor;
+
+
+public class CompanySearchSpider implements PageProcessor{
+	
+	private Site site = Site.me().setCharset("utf8").setRetryTimes(3).addHeader("Accept", "application/json, text/javascript, */*; q=0.01").addHeader("User-Agent", "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.87 Safari/537.36");
+	
+	private static final String originUrl = "http://www.kanzhun.com/autocomplete/searchkey.json?query=";
+	
+	
+	@Override
+	public void process(Page page){
+		String html = page.getJson().toString();
+		System.out.println(html);
+		JSONObject json = new JSONObject(html);
+		JSONArray company =json.getJSONArray("suggestions");
+		Iterator<Object> iterator = company.iterator();
+		while (iterator.hasNext()) {
+			JSONObject object = (JSONObject) iterator.next();
+			System.out.println(object.getString("data")+"  "+object.getString("value")+ "  " +object.getString("hlvalue") +"  "+object.getString("type"));
+		}
+	}
+
+	@Override
+	public Site getSite() {
+		// TODO Auto-generated method stub
+		return site;
+	}
+	
+	public static void run(String value){
+		String url = originUrl+ value + "&type=2";
+		Spider.create(new CompanySearchSpider()).addUrl(url).run();
+	}
+	
+	public static void main() {
+		run("网易");
+	}
+
+}

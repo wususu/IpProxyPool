e8699d4ea4b7291651aeb7363dc937d396478bb2
janke <keven_susu@126.com>
2017-05-16 23:59:31 +0800

    缓存业务逻辑

---
+++ src/main/java/service/CacheManagerService.java
@@ -0,0 +1,11 @@
+package service;
+
+import java.util.HashMap;
+
+import entity.Cache;
+
+public class CacheManagerService {
+	
+	private static HashMap<String, Cache> cachMap = new HashMap<String, Cache>();
+	
+}

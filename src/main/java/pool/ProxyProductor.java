cc164471c878d65e9a6898c314108eb06d112d21
janke <keven_susu@126.com>
2017-07-10 01:02:50 +0800

    代理生产者

---
+++ src/main/java/pool/ProxyProductor.java
@@ -0,0 +1,37 @@
+package pool;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
+
+import pool.spider.Spiders;
+
+@Component
+public class ProxyProductor implements Runnable{
+
+	public static Boolean runSignal = true;
+	
+	@Autowired
+	@Qualifier("xiciProxySpider")
+	private Spiders xiciProxySpider;
+	
+	@Autowired
+	@Qualifier("kuBoBoProxySpider")
+	private Spiders kuBoBoProxySpider;
+	
+	@Autowired
+	@Qualifier("kuaiProxySpider")
+	private Spiders kuaiProxySpider;
+	
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		if(runSignal){
+			runSignal = false;
+			xiciProxySpider.start();
+			kuBoBoProxySpider.start();
+			kuaiProxySpider.start();
+		}
+	}
+
+}

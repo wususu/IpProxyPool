179340dc4318520e3197e640f1f1db341eb62938
janke <keven_susu@126.com>
2017-07-10 01:04:58 +0800

    时间工具类

--- src/main/java/tools/DateFormater.java
+++ src/main/java/tools/DateFormater.java
@@ -10,11 +10,19 @@ public class DateFormater {
 	
 	private static SimpleDateFormat formater = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
 	
+	public static String getTime(){
+		return DateFormater.DateToString(new Date());
+	}
+	
 	public static String DateToString(Date date){
 		return formater.format(date);
 	}
 	
 	public static Date StringToDate(String string) throws ParseException{
 		return formater.parse(string);
+	}
+
+	public static void main(String[] args) {
+		DateFormater bb = new DateFormater();
 	}
 }

ec035fca07f74b29cf5b414c4fd987e8399ad986
janke <keven_susu@126.com>
2017-07-10 01:08:18 +0800

    抓取结果发送修改

--- src/main/java/pipeline/AssessPipeLine.java
+++ src/main/java/pipeline/AssessPipeLine.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import entity.Company;
+import service.CompanyAssessResultService;
 import service.CompanyService;
 import us.codecraft.webmagic.ResultItems;
 import us.codecraft.webmagic.Task;
@@ -20,8 +21,11 @@ public class AssessPipeLine implements Pipeline{
 	@Autowired
 	private CompanyService companyService;
 	
+	@Autowired
+	private CompanyAssessResultService companyAssessResultService;
+	
 	private String companyKey;
-
+    
 	public void setCompanyKey(String companyKey){
 		this.companyKey = companyKey;
 	}
@@ -42,12 +46,11 @@ public class AssessPipeLine implements Pipeline{
 	
 	public void process(ResultItems resultItems, Task task){
 		String percentage = resultItems.get("percentage");
-		System.out.println(percentage);
 		Company company = companyService.get(companyKey);
 		if (company != null && !percentage.isEmpty()) {
 			Double percentageDouble = getPercentageDouble(percentage);
 			company.setCompanyAssess(percentageDouble);
-			companyService.update(company);
+			companyAssessResultService.addResult(company);
 		}
 	}
 }
--- src/main/java/pipeline/SearchPipeLine.java
+++ src/main/java/pipeline/SearchPipeLine.java
@@ -1,6 +1,8 @@
 package pipeline;
 
+import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.List;
 
 import org.json.JSONArray;
 import org.json.JSONObject;
@@ -9,6 +11,7 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 import entity.Company;
+import service.CompanySearchResultService;
 import service.CompanyService;
 import us.codecraft.webmagic.ResultItems;
 import us.codecraft.webmagic.Task;
@@ -26,20 +29,31 @@ public class SearchPipeLine implements Pipeline{
 	@Autowired
 	@Qualifier("companyServiceImpl")
 	private CompanyService companyService;
+	
+	@Autowired
+	private CompanySearchResultService companySearchResultService;
 
 	@Override
 	public void process(ResultItems resultItems, Task task) {
 		Object companys = resultItems.get("companys");
 		if (companys != null) {
 			// 入库逻辑
+			List<Company> companyList = new ArrayList<>();
 			Iterator<Object> iterator =((JSONArray)companys).iterator();
 			while (iterator.hasNext()) {
 				JSONObject object = (JSONObject) iterator.next();
 				Integer key = object.getInt("data");
 				String name = object.getString("value");
+				System.out.println(name + "  " +key);
 				if (name != null && key.toString() != null ) {
-					companyService.add(new Company(name, key.toString()));
+					companyList.add(new Company(name, key.toString()));
 				}
+			}
+			try{
+				companySearchResultService.addResult(companyList);
+			}catch (Exception e) {
+				// TODO: handle exception
+				e.printStackTrace();
 			}
 		}
 
